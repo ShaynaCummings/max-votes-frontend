57672f2dc92915e3639c257e7120fc62dd88e5c9
Laura Vecchio <laura@maxwellhealth.com>
2015-07-23 13:46:17 -0400

    YASSSS POSTS

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -9,5 +9,17 @@
  */
 angular.module('maxVotes')
   .controller('MainCtrl', function ($scope) {
-    $scope.name = 'test';
+    $scope.posts = [
+		  {title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 15},
+		  {title: 'post 4', upvotes: 9},
+		  {title: 'post 5', upvotes: 4}
+		];
+
+    $scope.addPost = function(){
+    	if(!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.title = '';
+	};
   });
--- app/scripts/controllers/PostsCtrl.js
+++ app/scripts/controllers/PostsCtrl.js
@@ -11,12 +11,6 @@ angular.module('maxVotes')
 	.controller('PostsCtrl', [
 	'$scope',
 	function($scope){
-		$scope.posts = [
-		  {title: 'post 1', upvotes: 5},
-		  {title: 'post 2', upvotes: 2},
-		  {title: 'post 3', upvotes: 15},
-		  {title: 'post 4', upvotes: 9},
-		  {title: 'post 5', upvotes: 4}
-		];
+		$scope.name = 'test';
 	}
 ]);
