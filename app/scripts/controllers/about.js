bb91af64a482b9839949050678fc17c88a8332c2
Laura Vecchio <laura@maxwellhealth.com>
2015-07-23 10:58:57 -0400

    Yeoman angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name maxVotesFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the maxVotesFrontendApp
+ */
+angular.module('maxVotesFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
